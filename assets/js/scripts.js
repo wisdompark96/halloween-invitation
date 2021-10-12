f4682352c87e3cf87402958dd932efa20b06d011
wisdompark96 <39057317+wisdompark96@users.noreply.github.com>
2021-10-12 22:55:49 +0900

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,4 @@
 
-Splitting();
-
-$('.email').keyup(function(){
-   $( ".field p" ).remove();
-   var emailText = $('.email').val();
-   $(".field").append("<p class='text' data-splitting='chars'>" + emailText + "</p>");
-    Splitting();
-});
-
-$('.signup').submit(function(event){
- event.preventDefault();
-  $(".field").addClass("haunt");
-  var grave = ($(".field .text .word").width() + 40) + 'px';
-  $('body').get(0).style.setProperty("--width", grave);
-  setTimeout(function(){	
-    $('.email').val('');
-    $( ".field p" ).remove();
-    $(".field").removeClass("haunt");
-    $('body').get(0).style.setProperty("--width", "calc(100% + 200px)");
-    $(".field").append("<p class='text initial' data-splitting='chars'>email</p>");
-    Splitting();
-}, 7000);
-});
+$(document).ready(function() {
+$("#intro-text > h2").css('opacity',1).lettering('words').children("span").lettering().children("span").lettering(); 
+})
